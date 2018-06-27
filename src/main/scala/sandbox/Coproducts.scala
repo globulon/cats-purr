470fb301020ec9d94bf8463e3436b364330738e2
Marc-Daniel Ortega <globulon@gmail.com>
2018-06-27 23:25:11 +0100

    test with either

--- src/main/scala/sandbox/Coproducts.scala
+++ src/main/scala/sandbox/Coproducts.scala
@@ -1,6 +1,29 @@
 package sandbox
 
+import cats.Monad
+import cats.data.Validated
+import cats.data.Validated.{Invalid, Valid}
+import cats.kernel.Monoid
+
 
 trait Coproducts {
-  type ∪[+E, +A]  = Either[E, A]
+  type ∪[+E, +A]  = Validated[E, A]
+
+  implicit def monad[E : Monoid]:  Monad[∪[E, ?]] = new Monad[∪[E, ?]] {
+    override def pure[A](x: A): E ∪ A = Valid(x)
+
+    override def flatMap[A, B](fa: E ∪ A)(f: A ⇒ E ∪ B): E ∪ B = fa match {
+      case Valid(a)   ⇒ f(a)
+      case Invalid(e) ⇒ Invalid(e)
+    }
+
+    override def ap[A, B](ff: E ∪ (A ⇒ B))(fa: E ∪ A): E ∪ B = (ff, fa ) match {
+      case (Invalid(ee), Invalid(e))  ⇒ Invalid(implicitly[Monoid[E]].combine(ee, e))
+      case (Invalid(ee), _)           ⇒ Invalid(ee)
+      case (_, Invalid(e))            ⇒ Invalid(e)
+      case (Valid(f), Valid(a))       ⇒ Valid(f(a))
+    }
+
+    override def tailRecM[A, B](a: A)(f: A ⇒ E ∪ Either[A, B]): E ∪ B = ???
+  }
 }
--- src/main/scala/sandbox/SafeResults.scala
+++ src/main/scala/sandbox/SafeResults.scala
@@ -8,19 +8,6 @@ trait SafeResults {
   type Errors = List[ApplicationError]
   type Safe[A] = ∪[Errors, A]
 
-//
-//  implicit def safeApp:Applicative[Safe] = new Applicative[Safe] {
-//    override def pure[A](a: A): Safe[A] = Inject[A, Safe[A]].inj(a)
-//
-//    override def ap[A, B](ff: Safe[A ⇒ B])(fa: Safe[A]): Safe[B] = (ff, fa) match {
-//      case (Left(l), Left(ll)) ⇒ Left(l ++ ll)
-//      case (Left(l), _) ⇒ Left(l)
-//      case (_, Left(ll)) ⇒ Left(ll)
-//      case (Right(f), Right(a)) ⇒ Right(f(a))
-//    }
-//  }
-
-
   def safe[A](a: ⇒ A): Safe[A] = Try(a) match {
     case Failure(t) ⇒ Inject[Errors, Safe[A]].inj(List(Error(t.getMessage)))
     case Success(r) ⇒ Inject[A, Safe[A]].inj(r)
